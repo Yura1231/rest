56b00c77c658454b45e2f330e91c88baed46ab9f
SYMA <symovych.o@nltu.lviv.ua>
2025-02-27 21:24:45 +0200

    Added necessary files for the Django project

---
+++ project/posts/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Post
+from .forms import PostForm
+from django.contrib import messages
+
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Пост успішно додано!')
+            return redirect('post_list')
+    else:
+        form = PostForm()
+    return render(request, 'posts/create_post.html', {'form': form})
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'posts/post_list.html', {'posts': posts})
+
+def edit_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Пост успішно оновлено!')
+            return redirect('post_list')
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'posts/edit_post.html', {'form': form})
+
+def delete_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    messages.success(request, 'Пост успішно видалено!')
+    return redirect('post_list')
