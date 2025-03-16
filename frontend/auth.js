3f3f31236500891ba85a79e26d02d4ef3824e09a
Yura1231 <kvasnytsya.y@nltu.lviv.ua>
2025-03-16 11:18:47 +0200

    Add routs

---
+++ frontend/auth.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const accessToken = localStorage.getItem('access_token');
+   
+
+    if (accessToken) {
+        document.getElementById('register-btn').style.display = 'none';
+        document.getElementById('login-btn').style.display = 'none';
+        document.getElementById('logout-btn').style.display = 'inline-block';
+        document.getElementById('profile-btn').style.display = 'inline-block';
+        
+        
+    }
+
+    
+});
+
+document.getElementById('logout-btn').addEventListener('click', function () {
+    
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('refresh_token');
+    
+    location.reload();
+
+    
+    
+});
+
+document.getElementById('profile-btn').addEventListener('click', function() {
+    window.location.href = 'profile.html';
+});
